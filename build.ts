936d6c89ffe4af775c9da1189e5e1c1fc445afaa
Abdullah Ibne Atiq <hello@abdu.io>
2024-12-13 08:37:26 +0000

    Create build script

---
+++ build.ts
@@ -0,0 +1,56 @@
+import type { BuildConfig } from 'bun'
+import dts from 'bun-plugin-dts'
+
+const outdir = './dist'
+
+const buildConfig: BuildConfig = {
+  entrypoints: ['./index.ts'],
+  outdir,
+  minify: true,
+  sourcemap: 'linked',
+  plugins: [dts()],
+}
+
+const bundleEsm = async () => {
+  console.time('ESM build')
+  await Bun.build({ ...buildConfig, format: 'esm', naming: '[dir]/[name].js' })
+  console.timeLog('ESM build')
+}
+
+const bundleCjs = async () => {
+  console.time('CJS build')
+  await Bun.build({ ...buildConfig, format: 'cjs', naming: '[dir]/[name].cjs' })
+  console.timeLog('CJS build')
+}
+
+const exportJson = async () => {
+  console.time('JSON export')
+
+  const { sarf, sarfHelpers } = await import('./index')
+
+  // Export sarf as-is
+  await Bun.write(`${outdir}/sarf.json`, JSON.stringify(sarf))
+
+  // Utility for type-safe Object.entries()
+  const entries = <T extends object>(obj: T): [keyof T, T[keyof T]][] => {
+    return Object.entries(obj) as [keyof T, T[keyof T]][]
+  }
+
+  // Export a copy of sarf with replaced roots
+  for (const [verbType, chapters] of entries(sarf)) {
+    for (const [chapterKey, chapter] of entries(chapters)) {
+      if (chapter) {
+        sarf[verbType][chapterKey] = sarfHelpers.replaceRoots(chapter)
+      }
+    }
+  }
+
+  // Export a copy of sarf with replaced roots
+  await Bun.write(`${outdir}/sarf-archetypes.json`, JSON.stringify(sarf))
+
+  console.timeLog('JSON export')
+}
+
+await Promise.all([bundleEsm(), bundleCjs(), exportJson()])
+
+console.log('Build complete!')
--- index.ts
+++ index.ts
@@ -1,1 +1,4 @@
-console.log('Hello via Bun!')
+import sarf from './sarf'
+import sarfHelpers from './helpers'
+
+export { sarf, sarfHelpers }
